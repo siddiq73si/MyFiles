04c6d3e17364c591c994b42a1042f4cb146c9f4d
siddiq73si <sinthasiddiq@gmail.com>
2025-08-25 12:18:40 +0530

    file name changed

---
+++ ternaryoperator1.java
@@ -0,0 +1,27 @@
+//Example program of Ternary Operator
+
+class Example
+{	
+	public static void main(String args[])
+	{
+		int a=0;
+		a=5<4?5:4;
+		System.out.println("a = "+a);
+		
+		char c='M';
+		char d='D';
+		
+		System.out.println(c==d?"M and D is same":"M and D NotSame");
+		
+		
+		char e='M';
+		char f='m';
+		
+		System.out.println(e==f?"M and m is same":"M and m NotSame");
+		
+		char g='M';
+		char h='M';
+		
+		System.out.println(g==h?"M and M is same":"M and M NotSame");
+	}
+}
